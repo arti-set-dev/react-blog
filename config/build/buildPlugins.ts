ecdccdc505f693cad8c66a043e042611c79e6ee4
roman <r_kovtun@lweb.pro>
2024-10-30 21:14:07 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,6 +6,14 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+  if (isDev) {
+    return [
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
+    ];
+  }
+
   return [
     new HtmlWebpackPlugin({
       template: paths.html,
@@ -16,9 +24,6 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
     }),
   ];
 }
