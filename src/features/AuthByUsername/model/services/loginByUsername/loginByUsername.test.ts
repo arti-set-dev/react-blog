f14fdd5caad859a1ce9643d90e71066d02095dd0
roman <website.development593@gmail.com>
2025-01-23 12:53:36 +0300

    fix unit test

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -28,18 +28,18 @@ describe('loginByUsername.test', () => {
 
     expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
     expect(thunk.dispatch).toHaveBeenCalledTimes(3);
-    expect(thunk.api.post).toHaveBeenCalledWith('/common', { username: '123', password: '123' });
+    expect(thunk.api.post).toHaveBeenCalledWith('/login', { username: '123', password: '123' });
     expect(result.meta.requestStatus).toBe('fulfilled');
     expect(result.payload).toBe(userValue);
   });
 
-  test('rejected common', async () => {
+  test('rejected login', async () => {
     const thunk = new TestAsyncThunk(loginByUsername);
     thunk.api.post.mockRejectedValueOnce(new Error('Forbidden'));
     const result = await thunk.callThunk({ username: '123', password: '123' });
 
     expect(thunk.dispatch).toHaveBeenCalledTimes(2);
-    expect(thunk.api.post).toHaveBeenCalledWith('/common', { username: '123', password: '123' });
+    expect(thunk.api.post).toHaveBeenCalledWith('/login', { username: '123', password: '123' });
     expect(result.meta.requestStatus).toBe('rejected');
     expect(result.payload).toBe('error');
   });
