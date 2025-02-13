9a1e3169e738a793c13f58e4c08aeddf7ea058df
roman <website.development593@gmail.com>
2025-02-13 13:43:15 +0300

    redesign notification button component

--- src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
@@ -1,10 +1,15 @@
 import { useTranslation } from 'react-i18next';
 import { VStack } from '@/shared/ui/redesigned/Stack';
-import { AppLink } from '@/shared/ui/deprecated/AppLink';
-import { Text, TextSize } from '@/shared/ui/deprecated/Text';
+import { AppLink as AppLinkDeprecated } from '@/shared/ui/deprecated/AppLink';
+import { Text as TextDeprecated, TextSize } from '@/shared/ui/deprecated/Text';
+import { Text } from '@/shared/ui/redesigned/Text';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Notification } from '../../model/types/notification';
 import cl from './NotificationItem.module.scss';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { getVstack } from '@/shared/lib/stack/getVstack/getVstack';
+import { AppLink } from '@/shared/ui/redesigned/AppLink';
 
 interface NotificationItemProps {
   className?: string;
@@ -16,16 +21,35 @@ export const NotificationItem = (props: NotificationItemProps) => {
   const { t } = useTranslation();
 
   return (
-    <VStack
-      tag="li"
-      gap="4"
-      className={classNames(cl.NotificationItem, {}, [className])}
-    >
-      <Text size={TextSize.M}>{notification.title}</Text>
-      <Text>{notification.description}</Text>
-      {notification.href && (
-        <AppLink to={notification.href}>{t('Go to link')}</AppLink>
+    <ToggleFeatures
+      feature="isAppRedesigned"
+      on={(
+        <Card
+          tag="li"
+          max
+          offset="16"
+          className={classNames(getVstack({ gap: 8 }))}
+        >
+          <Text size="m">{notification.title}</Text>
+          <Text>{notification.description}</Text>
+          {notification.href && (
+            <AppLink to={notification.href}>{t('Go to link')}</AppLink>
+          )}
+        </Card>
       )}
-    </VStack>
+      off={(
+        <VStack
+          tag="li"
+          gap="4"
+          className={classNames(cl.NotificationItem, {}, [className])}
+        >
+          <TextDeprecated size={TextSize.M}>{notification.title}</TextDeprecated>
+          <TextDeprecated>{notification.description}</TextDeprecated>
+          {notification.href && (
+            <AppLinkDeprecated to={notification.href}>{t('Go to link')}</AppLinkDeprecated>
+          )}
+        </VStack>
+      )}
+    />
   );
 };
--- src/entities/Notification/ui/NotificationList/NotificationList.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.tsx
@@ -1,11 +1,12 @@
 import { useTranslation } from 'react-i18next';
 import { VStack } from '@/shared/ui/redesigned/Stack';
-import { Skeleton } from '@/shared/ui/deprecated/Skeleton';
-import { Text, TextTheme } from '@/shared/ui/deprecated/Text';
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { Skeleton as SkeletonDeprecated } from '@/shared/ui/deprecated/Skeleton';
+import { Text as TextDeprecated, TextTheme } from '@/shared/ui/deprecated/Text';
+import { Text } from '@/shared/ui/redesigned/Text';
 import { useNotificationList } from '../../api/notificationApi';
 import { NotificationItem } from '../../ui/NotificationItem/NotificationItem';
-import cl from './NotificationList.module.scss';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Skeleton } from '@/shared/ui/redesigned/Skeleton/Skeleton';
 
 interface NotificationListProps {
   className?: string;
@@ -24,35 +25,64 @@ export const NotificationList = (props: NotificationListProps) => {
 
   if (isLoading) {
     return (
-      <VStack
-        tag="div"
-        className={classNames(cl.NotificationList, {}, [className])}
-      >
-        <Skeleton width="100%" border="10px" />
-        <Skeleton width="100%" border="10px" />
-        <Skeleton width="100%" border="10px" />
-        <Skeleton width="100%" border="10px" />
-      </VStack>
+      <ToggleFeatures
+        feature="isAppRedesigned"
+        on={(
+          <VStack
+            gap="16"
+            tag="div"
+          >
+            <Skeleton width="100%" border="10px" />
+            <Skeleton width="100%" border="10px" />
+            <Skeleton width="100%" border="10px" />
+            <Skeleton width="100%" border="10px" />
+          </VStack>
+        )}
+        off={(
+          <VStack
+            gap="16"
+            tag="div"
+          >
+            <SkeletonDeprecated width="100%" border="10px" />
+            <SkeletonDeprecated width="100%" border="10px" />
+            <SkeletonDeprecated width="100%" border="10px" />
+            <SkeletonDeprecated width="100%" border="10px" />
+          </VStack>
+        )}
+      />
     );
   }
 
   if (error) {
     return (
-      <VStack
-        tag="div"
-        className={classNames(cl.NotificationList, {}, [className])}
-      >
-        <Text theme={TextTheme.ERROR}>
-          {t('There was an error loading notifications')}
-        </Text>
-      </VStack>
+      <ToggleFeatures
+        feature="isAppRedesigned"
+        on={(
+          <VStack
+            tag="div"
+          >
+            <Text variant="error">
+              {t('There was an error loading notifications')}
+            </Text>
+          </VStack>
+        )}
+        off={(
+          <VStack
+            tag="div"
+          >
+            <TextDeprecated theme={TextTheme.ERROR}>
+              {t('There was an error loading notifications')}
+            </TextDeprecated>
+          </VStack>
+        )}
+      />
     );
   }
 
   return (
     <VStack
       tag="ul"
-      className={classNames(cl.NotificationList, {}, [className])}
+      gap="16"
     >
       {notifications?.map((notification) => (
         <NotificationItem notification={notification} key={notification.id} />
