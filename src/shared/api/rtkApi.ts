37a1337a8bc7d098fed66132f8ecbebbb4c513e4
roman <website.development593@gmail.com>
2025-03-02 11:17:00 +0300

    added a request for articles for non authorized users

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,16 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
 
-// Define a service using a base URL and expected endpoints
 export const rtkApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: __API__,
+    baseUrl: __API__ || 'https://default-api.com',
     prepareHeaders: (headers) => {
-      const token = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+      const token = localStorage.getItem(USER_LOCALSTORAGE_KEY);
 
       if (token) {
-        headers.set('Authorization', token);
+        headers.set('Authorization', `Bearer ${token}`);
       }
 
       return headers;
