09e6dcf9ed1c2ea99ca4d875a754a65acd5b35d1
roman <website.development593@gmail.com>
2025-03-11 18:02:40 +0300

    add back button for ArticleEditPage, add StickyContentLayout

--- src/pages/ArticleEditPage/ui/ArticleEditPage.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage.tsx
@@ -1,11 +1,15 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Page } from '@/widgets/Page';
 import cl from './ArticleEditPage.module.scss';
 import { ArticleCreate } from '@/features/articleCreate';
 import { ArticleEdit } from '@/features/articleEdit';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { StickyContentLayout } from '@/shared/layouts/SticlyContentLayout';
+import { Button } from '@/shared/ui/redesigned/Button';
+import { getRouteArticleDetails } from '@/shared/const/router';
 
 interface ArticleEditPageProps {
   className?: string;
@@ -16,15 +20,38 @@ const ArticleEditPage = memo((props: ArticleEditPageProps) => {
   const { t } = useTranslation();
   const { id } = useParams<{ id: string }>();
   const isEdit = Boolean(id);
+  const navigate = useNavigate();
+
+  const onBackToArticle = () => {
+    navigate(getRouteArticleDetails(id ?? ''));
+  };
 
   return (
-    <Page className={classNames(cl.ArticleEditPage, {}, [className])}>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
       {isEdit ? (
-        <ArticleEdit id={id} />
+        <ToggleFeatures
+          feature="isAppRedesigned"
+          on={(
+            <StickyContentLayout
+              left={<Button variant="outline" onClick={onBackToArticle}>{t('Back')}</Button>}
+              content={(
+                <Page className={classNames(cl.ArticleEditPage, {}, [className])}>
+                  <ArticleEdit id={id} />
+                </Page>
+              )}
+            />
+          )}
+          off={(
+            <Page className={classNames(cl.ArticleEditPage, {}, [className])}>
+              <ArticleEdit id={id} />
+            </Page>
+          )}
+        />
       ) : (
         <ArticleCreate />
       )}
-    </Page>
+    </>
   );
 });
 
